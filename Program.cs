0a2830f4e4253b5aca39811f0517cbbff0915ea3
Siyavush-L <s.lazgiyev@gmail.com>
2023-04-11 23:03:43 +0600

    Zadacha34

--- Program.cs
+++ Program.cs
@@ -4,22 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] nums = new int[10]; // создаем массив с 10 элементами
-        Random rnd = new Random(); // создаем генератор случайных чисел
+        Console.Write("Введите количество элементов в массиве: ");
+        int size = int.Parse(Console.ReadLine()); // ввод размера массива
 
-        Console.WriteLine("Исходный массив:");
-        for (int i = 0; i < nums.Length; i++)
+        int[] nums = new int[size]; // создание массива заданного размера
+        Console.WriteLine($"Введите {size} элементов массива:");
+
+        // заполнение массива элементами, введенными пользователем
+        for (int i = 0; i < size; i++)
         {
-            nums[i] = rnd.Next(100); // заполняем массив случайными числами от 0 до 99
-            Console.Write($"{nums[i]} ");
+            nums[i] = int.Parse(Console.ReadLine());
         }
 
-        int sum = 0;
-        for (int i = 1; i < nums.Length; i += 2) // начинаем с 1, т.к. индексы массива начинаются с 0
+        // подсчет количества четных чисел в массиве
+        int evenCount = 0;
+        foreach (int num in nums)
         {
-            sum += nums[i];
+            if (num % 2 == 0)
+            {
+                evenCount++;
+            }
         }
 
-        Console.WriteLine($"\nСумма элементов на нечетных позициях: {sum}");
+        Console.WriteLine($"Количество четных чисел в массиве: {evenCount}.");
     }
 }
